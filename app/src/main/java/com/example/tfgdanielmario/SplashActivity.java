adf971d06cc03bc2b7be3fb8024bd93314f30fcf
Dani <danielblanco2211@gmail.com>
2025-06-01 12:30:34 +0200

    App acabada

---
+++ app/src/main/java/com/example/tfgdanielmario/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.tfgdanielmario;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_DURATION = 3500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Iniciar la animación
+        ImageView logoImage = findViewById(R.id.splash_logo);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logoImage.startAnimation(fadeIn);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        }, SPLASH_DURATION);
+    }
+} 
